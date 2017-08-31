47aaff731d8229bc3b87f0f04ad7e31c08c78f73
linkcoding <kenny.lin.91@gmail.com>
2017-08-31 11:45:54 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,19 @@
+class Spaceship {
+  constructor(name, crewMembers, phasers, shields) {
+    this.name = name
+    this.crewMembers = this.addCrewMembersToShip(crewMembers)
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.docked = crewMembers.length === 0 ? true : false
+    this.phasersCharge = 'uncharged'
+  }
+  addCrewMembersToShip(crew) {
+    debugger
+    return crew.map(member => {
+      member.currentShip = this
+      return member
+    })
+  }
+}
